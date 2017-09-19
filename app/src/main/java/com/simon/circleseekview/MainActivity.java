965c51382aa904e7098f5cb84a3b3b59eccc19d1
ZHUQI <312282860@qq.com>
2017-09-19 14:58:13 +0800

    添加进度文字显示

--- app/src/main/java/com/simon/circleseekview/MainActivity.java
+++ app/src/main/java/com/simon/circleseekview/MainActivity.java
@@ -3,24 +3,28 @@ package com.simon.circleseekview;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CircleSeekView k2_light_color_picker;
-    private ImageView k2_light_small_icon;
+    private CircleSeekView color_picker;
+    private RelativeLayout layout;
+    private TextView value_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        k2_light_color_picker = findViewById(R.id.k2_light_color_picker);
-        k2_light_small_icon = findViewById(R.id.k2_light_small_icon);
-        k2_light_color_picker.setColorValue(35);
-        k2_light_color_picker.setOnSelectColorListener(new CircleSeekView.OnSelectColorListener() {
+        color_picker = findViewById(R.id.k2_light_color_picker);
+        value_tv = findViewById(R.id.value_tv);
+        layout = findViewById(R.id.layout);
+        color_picker.setColorValue(35);
+        color_picker.setOnSelectColorListener(new CircleSeekView.OnSelectColorListener() {
             @Override
             public void onSelectColor(int[] rgb, int colorValue,boolean isFromUser) {
-                k2_light_small_icon.setBackgroundColor(Color.argb(255,rgb[0],rgb[1],rgb[2]));
+                layout.setBackgroundColor(Color.argb(255,rgb[0],rgb[1],rgb[2]));
+                value_tv.setText(colorValue+"");
             }
 
             @Override
@@ -34,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        k2_light_color_picker.recycleBitMap();
+        color_picker.recycleBitMap();
         super.onDestroy();
     }
 }
